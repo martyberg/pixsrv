3a36db1c907fd6929fa2fb8cd76b1de6c6e83b7d
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2020-08-24 16:23:57 +0200

    Fix settings chunk loading
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- apps/settings/webpack.js
+++ apps/settings/webpack.js
@@ -5,17 +5,16 @@ module.exports = {
 		'settings-apps-users-management': path.join(__dirname, 'src', 'main-apps-users-management'),
 		'settings-admin-security': path.join(__dirname, 'src', 'main-admin-security'),
 		'settings-personal-security': path.join(__dirname, 'src', 'main-personal-security'),
-		'settings-personal-webauthn': path.join(__dirname, 'src', 'main-personal-webauth')
+		'settings-personal-webauthn': path.join(__dirname, 'src', 'main-personal-webauth'),
 	},
 	output: {
 		path: path.resolve(__dirname, './js'),
 		publicPath: '/js/',
-		filename: 'vue-[name].js?v=[chunkhash]',
-		jsonpFunction: 'webpackJsonpSettings'
+		filename: 'vue-[name].js',
 	},
 	optimization: {
 		splitChunks: {
 			automaticNameDelimiter: '-',
-		}
-	}
+		},
+	},
 }
